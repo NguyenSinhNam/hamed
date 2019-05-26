613a9a107b4396f8e536e5fc2a0e7489a0a937fb
unknown <nguyensinhnam@gmail.com>
2019-05-26 14:01:08 +0700

    add html folder van-chuyen

--- javascript/main.js
+++ javascript/main.js
@@ -188,6 +188,67 @@
 
     }
 
+  var ToggleSlide = function() {
+    $('.info-logistic-detail .detail-box .title .icon-down').on('click', function(){
+      $(this).closest('.title').toggleClass('active');
+      $(this).closest('.detail-box').children('.detail-content').slideToggle();
+    })
+  };
+
+  var ShowPopup = function() {
+    var popupFinish = $('.btn-confirm.finish a');
+    var popupFinishClose = $('.box-confirm-order .delete');
+
+    // Course
+    popupFinish.on('click',function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        $(this).closest('.boxed').children('.box-confirm-order').addClass('open');
+        $('body').append('<div class="modal-backdrop fade show"></div>');
+    });
+    popupFinishClose.on('click', function(){
+        $(this).closest('.boxed').children('.box-confirm-order').removeClass('open');
+        $('.modal-backdrop.fade.show').remove();
+    });
+    $('.box-confirm-order').on('click', function(e){
+        e.stopPropagation();
+    });
+
+    jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
+  };
+
   var removePreloader = function() {
     $('.box-change-pw').hide();
     $(document).on('click', '.bt-change-pw', function() {
@@ -216,7 +277,10 @@
   $(function() {
     tabLogistic();
     CheckboxShowHideAll();
+    ToggleSlide();
+    ShowPopup();
     removePreloader();
   });
 
 })(jQuery);
+ 
